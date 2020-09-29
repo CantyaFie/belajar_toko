46ca1edbbed416eea84dda7d4cf322db2e1ba97f
Cantya Fie <cantyafiemasitha@fmail.com>
2020-09-29 11:15:22 +0700

    Menambah autensikasi API dengan JWT

--- app/User.php
+++ app/User.php
@@ -33,7 +33,13 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -16,18 +16,23 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/customer', 'CustomerController@show');
-Route::post('/customer', 'CustomerController@store');
-Route::put('/customer/{id}', 'CustomerController@update');
-Route::delete('/customer/{id_customer}', 'CustomerController@destroy');
+Route::post('/register', 'UserController@register');
+Route::post('/login', 'UserController@login');
+Route::group(['middleware' => ['jwt.verify']], function ()
+{
+    Route::get('/customer', 'CustomerController@show');
+    Route::post('/customer', 'CustomerController@store');
+    Route::put('/customer/{id}', 'CustomerController@update');
+    Route::delete('/customer/{id_customer}', 'CustomerController@destroy');
 
-Route::get('/product', 'ProductController@show');
-Route::post('/product', 'ProductController@store');
-Route::put('/product/{id}', 'ProductController@update');
-Route::delete('/product/{id_product}', 'ProductController@destroy');
+    Route::get('/product', 'ProductController@show');
+    Route::post('/product', 'ProductController@store');
+    Route::put('/product/{id}', 'ProductController@update');
+    Route::delete('/product/{id_product}', 'ProductController@destroy');
 
-Route::get('/orders', 'OrdersController@show');
-Route::get('/orders/{id_orders}', 'OrdersController@detail');
-Route::post('/orders', 'OrdersController@store');
-Route::put('/orders/{id}', 'OrdersController@update');
-Route::delete('/orders/{id}', 'ordersController@destroy');
+    Route::get('/orders', 'OrdersController@show');
+    Route::get('/orders/{id_orders}', 'OrdersController@detail');
+    Route::post('/orders', 'OrdersController@store');
+    Route::put('/orders/{id}', 'OrdersController@update');
+    Route::delete('/orders/{id}', 'ordersController@destroy');
+}
